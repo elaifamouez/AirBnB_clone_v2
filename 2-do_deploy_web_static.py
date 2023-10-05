763cc3d2502886fd97f0e89d39517970fd499e3f
mouezelaifa <mouezelaifa@gmail.com>
2023-10-05 13:04:37 -0700

    AirBnB Clone V2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,50 +1,93 @@
 #!/usr/bin/python3
-""" module doc
 """
-from fabric.api import task, local, env, put, run
-from datetime import datetime
+Fabric script that distributes an archive to web servers
+"""
+from fabric.api import run, put, env, sudo
 import os
 
+# Define hosts and user
 env.hosts = ['100.26.156.69', '54.174.114.135']
+env.user = "ubuntu"
 
 
-@task
-def do_pack():
-    """ method doc
-        sudo fab -f 1-pack_web_static.py do_pack
-    """
-    formatted_dt = datetime.now().strftime('%Y%m%d%H%M%S')
-    mkdir = "mkdir -p versions"
-    path = "versions/web_static_{}.tgz".format(formatted_dt)
-    print("Packing web_static to {}".format(path))
-    if local("{} && tar -cvzf {} web_static".format(mkdir, path)).succeeded:
-        return path
-    return None
-
-
-@task
 def do_deploy(archive_path):
-    """ method doc
-        fab -f 2-do_deploy_web_static.py do_deploy:
-        archive_path=versions/web_static_20231004201306.tgz
-        -i ~/.ssh/id_rsa -u ubuntu
     """
+    Distributes an archive to web servers
+
+    Args:
+        archive_path: Path to where the archive is stored locally
+
+    Returns:
+        True: If all operations have been done correctly
+        False: If any operation fails, like if archive_path doesn't exist
+    """
+    # Make sure archive path exists
+    if not os.path.isfile(archive_path):
+        return False
+
     try:
-        if not os.path.exists(archive_path):
+        # Get archive file (without leading path)
+        archive_file = os.path.basename(archive_path)
+
+        # Get folder name
+        folder = archive_file.split('.')[0]
+
+        # Extraction path (where archive will be extracted)
+        extract_path = f"/data/web_static/releases/{folder}"
+
+        # Upload the archive to /tmp/ folder of remote server
+        upload = put(archive_path, '/tmp/')
+
+        # Check if upload failed
+        if upload.failed:
             return False
-        fn_with_ext = os.path.basename(archive_path)
-        fn_no_ext, ext = os.path.splitext(fn_with_ext)
-        dpath = "/data/web_static/releases/"
-        put(archive_path, "/tmp/")
-        run("rm -rf {}{}/".format(dpath, fn_no_ext))
-        run("mkdir -p {}{}/".format(dpath, fn_no_ext))
-        run("tar -xzf /tmp/{} -C {}{}/".format(fn_with_ext, dpath, fn_no_ext))
-        run("rm /tmp/{}".format(fn_with_ext))
-        run("mv {0}{1}/web_static/* {0}{1}/".format(dpath, fn_no_ext))
-        run("rm -rf {}{}/web_static".format(dpath, fn_no_ext))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {}{}/ /data/web_static/current".format(dpath, fn_no_ext))
+
+        # Create folder in remote server
+        create = run(f"mkdir -p {extract_path}")
+
+        # Check folder creation
+        if create.failed:
+            return False
+
+        # Uncompress the archive to the folder /data/web_static/releases
+        extract = run(f"tar -xzf /tmp/{archive_file} -C {extract_path}/")
+
+        # Check if extraction failed
+        if extract.failed:
+            return False
+
+        # Delete the archive from remote server
+        del_archive = run(f"sudo rm /tmp/{archive_file}")
+
+        if del_archive.failed:
+            return False
+
+        # Move extracted content to new folder
+        move_extracted = run(f"mv {extract_path}/web_static/* {extract_path}/")
+
+        if move_extracted.failed:
+            return False
+
+        # Remove extracted folder (web_static) in remote server
+        del_folder = run(f"sudo rm -rf {extract_path}/web_static")
+
+        if del_folder.failed:
+            return False
+
+        # Delete symbolic link (/data/web_static/current) from remote server
+        del_sym = run('sudo rm -rf /data/web_static/current')
+
+        if del_sym.failed:
+            return False
+
+        # Create new symbolic link (/data/web_static/current)
+        new_sym = run(f"sudo ln -s {extract_path}/ /data/web_static/current")
+
+        if new_sym.failed:
+            return False
+
         print("New version deployed!")
+
         return True
     except Exception:
         return False
