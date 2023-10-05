f80235ae4d6d513e2d622246041539303fd8f9b3
mouezelaifa <mouezelaifa@gmail.com>
2023-10-05 13:20:59 -0700

    AirBnB Clone V2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,93 +1,49 @@
 #!/usr/bin/python3
-"""
-Fabric script that distributes an archive to web servers
-"""
-from fabric.api import run, put, env, sudo
-import os
+# Fabfile to distribute an archive to a web server.
+import os.path
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
 
-# Define hosts and user
-env.hosts = ['100.26.156.69', '54.174.114.135']
-env.user = "ubuntu"
+env.hosts = ["100.26.156.69", "54.174.114.135"]
 
 
 def do_deploy(archive_path):
-    """
-    Distributes an archive to web servers
+    """Distributes an archive to a web server.
 
     Args:
-        archive_path: Path to where the archive is stored locally
-
+        archive_path (str): The path of the archive to distribute.
     Returns:
-        True: If all operations have been done correctly
-        False: If any operation fails, like if archive_path doesn't exist
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
     """
-    # Make sure archive path exists
-    if not os.path.isfile(archive_path):
+    if os.path.isfile(archive_path) is False:
         return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
 
-    try:
-        # Get archive file (without leading path)
-        archive_file = os.path.basename(archive_path)
-
-        # Get folder name
-        folder = archive_file.split('.')[0]
-
-        # Extraction path (where archive will be extracted)
-        extract_path = f"/data/web_static/releases/{folder}"
-
-        # Upload the archive to /tmp/ folder of remote server
-        upload = put(archive_path, '/tmp/')
-
-        # Check if upload failed
-        if upload.failed:
-            return False
-
-        # Create folder in remote server
-        create = run(f"mkdir -p {extract_path}")
-
-        # Check folder creation
-        if create.failed:
-            return False
-
-        # Uncompress the archive to the folder /data/web_static/releases
-        extract = run(f"tar -xzf /tmp/{archive_file} -C {extract_path}/")
-
-        # Check if extraction failed
-        if extract.failed:
-            return False
-
-        # Delete the archive from remote server
-        del_archive = run(f"sudo rm /tmp/{archive_file}")
-
-        if del_archive.failed:
-            return False
-
-        # Move extracted content to new folder
-        move_extracted = run(f"mv {extract_path}/web_static/* {extract_path}/")
-
-        if move_extracted.failed:
-            return False
-
-        # Remove extracted folder (web_static) in remote server
-        del_folder = run(f"sudo rm -rf {extract_path}/web_static")
-
-        if del_folder.failed:
-            return False
-
-        # Delete symbolic link (/data/web_static/current) from remote server
-        del_sym = run('sudo rm -rf /data/web_static/current')
-
-        if del_sym.failed:
-            return False
-
-        # Create new symbolic link (/data/web_static/current)
-        new_sym = run(f"sudo ln -s {extract_path}/ /data/web_static/current")
-
-        if new_sym.failed:
-            return False
-
-        print("New version deployed!")
-
-        return True
-    except Exception:
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
+    return True
