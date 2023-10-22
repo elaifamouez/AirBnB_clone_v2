6a9acb8edfb04191efee4972b399f76127c4e8f3
mouezelaifa <mouezelaifa@gmail.com>
2023-10-22 05:38:04 -0700

    0x04. AirBnB clone - Web framework

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,65 +1,56 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
+""" script that starts a Flask web application """
 
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'.
-    /hbnb: Displays 'HBNB'.
-    /c/<text>: Displays 'C' followed by the value of <text>.
-    /python/(<text>): Displays 'Python' followed by the value of <text>.
-    /number/<n>: Displays 'n is a number' only if <n> is an integer.
-    /number_template/<n>: Displays an HTML page only if <n> is an integer.
-"""
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
+app.url_map.strict_slashes = False
 
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
+@app.route("/")
+def hello():
+    """/: displays Hello HBNB!"""
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route("/hbnb")
 def hbnb():
-    """Displays 'HBNB'"""
+    """/hbnb: displays HBNB"""
     return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def c(text):
-    """Displays 'C' followed by the value of <text>
-
-    Replaces any underscores in <text> with slashes.
-    """
+@app.route("/c/<text>")
+def C_text(text):
+    """/c/<text>: display C followed by the value of the text variable"""
     text = text.replace("_", " ")
     return "C {}".format(text)
 
 
-@app.route("/python", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python(text="is cool"):
-    """Displays 'Python' followed by the value of <text>
-
-    Replaces any underscores in <text> with slashes.
+@app.route("/python/", defaults={"text": "is cool"})
+@app.route("/python/<text>")
+def python_text(text):
+    """
+    /python/<text>: display Python followed by the value
+    of the text variable
     """
     text = text.replace("_", " ")
     return "Python {}".format(text)
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
-def number(n):
-    """Displays 'n is a number' only if <n> is an integer."""
+@app.route("/number/<int:n>")
+def number_n(n):
+    """/number/<n>: display n is a number only if n is an integer"""
     return "{} is a number".format(n)
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def number_template(n):
-    """Displays an HTML page only if <n> is an integer."""
+@app.route("/number_template/<int:n>")
+def number_template_n(n):
+    """
+    /number_template/<n>: display n is a number only if n is an integer
+        H1 tag: Number: n inside the tag BODY
+    """
     return render_template("5-number.html", n=n)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
