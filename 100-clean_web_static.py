0e92a74bca11af126c8b7fae02b6feda893bee7b
mouezelaifa <mouezelaifa@gmail.com>
2023-11-06 09:56:48 -0800

    0x03. AirBnB clone - Deploy static

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,82 +1,61 @@
 #!/usr/bin/python3
-""" module doc
-"""
-from fabric.api import task, local, env, put, run, runs_once
+""" Fabric script that generates a .tgz archive """
+from fabric.decorators import task
+from fabric.api import *
 from datetime import datetime
 import os
 
-env.hosts = ['100.26.156.69', '54.174.114.135']
+env.hosts = ["100.26.156.69", "54.174.114.135"]
 
 
-@runs_once
-def do_pack():
-    """ method doc
-        sudo fab -f 1-pack_web_static.py do_pack
-    """
-    formatted_dt = datetime.now().strftime('%Y%m%d%H%M%S')
-    mkdir = "mkdir -p versions"
-    path = "versions/web_static_{}.tgz".format(formatted_dt)
-    print("Packing web_static to {}".format(path))
-    if local("{} && tar -cvzf {} web_static".format(mkdir, path)).succeeded:
-        return path
-    return None
+@task
+def do_clean(number=0):
+    """formats input and cleans remote"""
+    n = 1
+    if int(number) != 0:
+        n = int(number)
+    pth = "/data/web_static/releases/*"
+    local("ls -dt ./versions/* | head -n -{} | xargs rm -fr".format(n))
+    run("ls -dt {} | head -n -{} | xargs rm -fr".format(pth, n))
+
+
+@task
+def deploy():
+    """ Fabric script that creates and distributes an archive """
+    file_name = do_pack()
+    if file_name is None:
+        return False
+    return do_deploy(file_name)
 
 
 @task
 def do_deploy(archive_path):
-    """ method doc
-        fab -f 2-do_deploy_web_static.py do_deploy:
-        archive_path=versions/web_static_20231004201306.tgz
-        -i ~/.ssh/id_rsa -u ubuntu
-    """
+    """Fabric script that distributes an archive to web servers"""
     try:
         if not os.path.exists(archive_path):
             return False
-        fn_with_ext = os.path.basename(archive_path)
-        fn_no_ext, ext = os.path.splitext(fn_with_ext)
-        dpath = "/data/web_static/releases/"
+        with_ext = archive_path.split("/")[-1]
+        without_ext = archive_path.split("/")[-1].split(".")[0]
+        pth = "/data/web_static/releases/"
         put(archive_path, "/tmp/")
-        run("rm -rf {}{}/".format(dpath, fn_no_ext))
-        run("mkdir -p {}{}/".format(dpath, fn_no_ext))
-        run("tar -xzf /tmp/{} -C {}{}/".format(fn_with_ext, dpath, fn_no_ext))
-        run("rm /tmp/{}".format(fn_with_ext))
-        run("mv {0}{1}/web_static/* {0}{1}/".format(dpath, fn_no_ext))
-        run("rm -rf {}{}/web_static".format(dpath, fn_no_ext))
+        run("mkdir -p " + pth + without_ext)
+        run("tar -xzf /tmp/{} -C {}{}/".format(with_ext, pth, without_ext))
+        run("rm /tmp/{}".format(with_ext))
+        run("mv {1}{0}/web_static/* {1}{0}/".format(without_ext, pth))
+        run("rm -rf {}{}/web_static".format(pth, without_ext))
         run("rm -rf /data/web_static/current")
-        run("ln -s {}{}/ /data/web_static/current".format(dpath, fn_no_ext))
-        print("New version deployed!")
+        run("ln -s {}{}/ /data/web_static/current".format(pth, without_ext))
         return True
     except Exception:
         return False
 
 
-@task
-def deploy():
-    """ method doc
-        sudo fab -f 1-pack_web_static.py do_pack
-    """
-    path = do_pack()
-    if path is None:
-        return False
-    return do_deploy(path)
-
-
 @runs_once
-def remove_local(number):
-    """ method doc
-        sudo fab -f 1-pack_web_static.py do_pack
-    """
-    local("ls -dt versions/* | tail -n +{} | sudo xargs rm -fr".format(number))
-
-
-@task
-def do_clean(number=0):
-    """ method doc
-        sudo fab -f 1-pack_web_static.py do_pack
-    """
-    if int(number) == 0:
-        number = 1
-    number = int(number) + 1
-    remove_local(number)
-    rem_path = "/data/web_static/releases/*"
-    run("ls -dt {} | tail -n +{} | sudo xargs rm -fr".format(rem_path, number))
+def do_pack():
+    """generates a .tgz archive from web_static"""
+    datestr = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_name = "versions/web_static_{}.tgz".format(datestr)
+    local("mkdir -p versions")
+    if local("tar -cvzf {} web_static/".format(file_name)).succeeded:
+        return file_name
+    return None
