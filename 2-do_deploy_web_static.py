58f7b4c95c9e8a3087a79b2c0dab63b4586ad49d
mouezelaifa <mouezelaifa@gmail.com>
2023-11-06 09:26:52 -0800

    0x03. AirBnB clone - Deploy static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,41 +1,40 @@
 #!/usr/bin/python3
-""" Fabric script that generates a .tgz archive """
-from fabric.decorators import task
-from fabric.api import *
+""" Function that compress a folder """
 from datetime import datetime
+from fabric.api import *
+import shlex
 import os
 
-env.hosts = ["100.27.4.102", "54.165.197.71"]
+
+env.hosts = ['44.211.97.124', '34.228.52.136']
+env.user = "ubuntu"
 
 
-@task
 def do_deploy(archive_path):
-    """Fabric script that distributes an archive to web servers"""
+    """ Deploys """
+    if not os.path.exists(archive_path):
+        return False
     try:
-        if not os.path.exists(archive_path):
-            return False
-        with_ext = archive_path.split("/")[-1]
-        without_ext = archive_path.split("/")[-1].split(".")[0]
-        pth = "/data/web_static/releases/"
+        name = archive_path.replace('/', ' ')
+        name = shlex.split(name)
+        name = name[-1]
+
+        wname = name.replace('.', ' ')
+        wname = shlex.split(wname)
+        wname = wname[0]
+
+        releases_path = "/data/web_static/releases/{}/".format(wname)
+        tmp_path = "/tmp/{}".format(name)
+
         put(archive_path, "/tmp/")
-        run("mkdir -p " + pth + without_ext)
-        run("tar -xzf /tmp/{} -C {}{}/".format(with_ext, pth, without_ext))
-        run("rm /tmp/{}".format(with_ext))
-        run("mv {1}{0}/web_static/* {1}{0}/".format(without_ext, pth))
-        run("rm -rf {}{}/web_static".format(pth, without_ext))
+        run("mkdir -p {}".format(releases_path))
+        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
+        run("rm {}".format(tmp_path))
+        run("mv {}web_static/* {}".format(releases_path, releases_path))
+        run("rm -rf {}web_static".format(releases_path))
         run("rm -rf /data/web_static/current")
-        run("ln -s {}{}/ /data/web_static/current".format(pth, without_ext))
+        run("ln -s {} /data/web_static/current".format(releases_path))
+        print("New version deployed!")
         return True
     except Exception:
         return False
-
-
-@task
-def do_pack():
-    """generates a .tgz archive from web_static"""
-    datestr = datetime.now().strftime("%Y%m%d%H%M%S")
-    file_name = "versions/web_static_{}.tgz".format(datestr)
-    local("mkdir -p versions")
-    if local("tar -cvzf {} web_static/".format(file_name)).succeeded:
-        return file_name
-    return None
